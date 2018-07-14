90eba6e04aa12a5c6b7424c1f51d701120cb06c5
Jasmine <obviously0utraged@yahoo.com>
2018-07-14 18:00:29 -0400

    Multiple screens sharing data, general UI

--- Kivy.py
+++ Kivy.py
@@ -4,20 +4,27 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.popup import Popup
 from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
 
 
 kivy.require('1.10.0')
+global user
+
+class UserInfoScreen(Screen):
+    pass
 
 class MainScreen(Screen):
+
     def send_username(self):
-        self.ids.chatroom.text = "Username was sent"
-        self.ids.chatroom.text = "Username is " + self.ids.msg_input.text
+        user = self.ids.username.text
 
     def send_audio(self):
         pass
 
 class ScreenManagement(ScreenManager):
-    pass
+    username = StringProperty("")
+    audience_member = ObjectProperty(True)
+    player = ObjectProperty(False)
 
 
 sm = Builder.load_file("JamSesh.kv")
