f41e6acc047880ebe89a9167372cf3fd3951c417
Jasmine <obviously0utraged@yahoo.com>
2018-07-17 21:57:39 -0400

    Basic chat with some errors server-side working!

--- Kivy.py
+++ Kivy.py
@@ -2,21 +2,72 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.uix.popup import Popup
 from kivy.properties import StringProperty
 from kivy.properties import ObjectProperty
+import socket, threading
+import pickle
+
 
 
 kivy.require('1.10.0')
-global user
+global username
+global s
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 
 class UserInfoScreen(Screen):
     pass
 
 class MainScreen(Screen):
 
-    def send_username(self):
-        user = self.ids.username.text
+    global s
+    username = ""
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        #self.messages = self.ids["messages"]
+
+    def on_enter(self):
+        s.connect(('localhost', 9000))
+        self.ids.chatroom.text = "You are connected to the chat! \n"
+        threading.Thread(target=self.handle_messages).start()
+
+
+    def send_message(self, message_to_send):
+        try:
+            temp = message_to_send.split(" ")
+            username = temp[0]
+
+            msg_without_username = temp[1:]
+
+            print(msg_without_username)
+
+            message_to_send_text = " ".join(msg_without_username)
+
+            print(message_to_send_text)
+
+            user_and_message = [username, message_to_send_text]
+
+            total_data = pickle.dumps(user_and_message)
+
+            s.sendto(total_data, ('localhost', 9000))
+            self.ids.chatroom.text += '\n' + username + " > " + message_to_send_text
+
+        except Exception as e:
+            print("Error sending: ", e)
+
+
+    def handle_messages(self):
+        while True:
+            try:
+                data = s.recv(1024)
+                data_list = pickle.loads(data)
+                self.ids.chatroom.text += str('\n' + data_list[0]) + ' > ' + str(data_list[1])
+            except Exception as e:
+                print(e)
+
 
     def send_audio(self):
         pass
@@ -27,10 +78,12 @@ class ScreenManagement(ScreenManager):
     player = ObjectProperty(False)
 
 
+
 sm = Builder.load_file("JamSesh.kv")
 
 class JamSeshApp(App):
     def build(self):
         return sm
 
-JamSeshApp().run()
+if __name__ == "__main__":
+    JamSeshApp().run()
